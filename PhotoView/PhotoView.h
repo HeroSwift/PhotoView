a0692d2dd0c6e6d7449d10107825dc97a445b390
musicode <musicode@qq.com>
2018-12-14 12:00:35 +0800

    Initial Commit

---
+++ PhotoView/PhotoView.h
@@ -0,0 +1,19 @@
+//
+//  PhotoView.h
+//  PhotoView
+//
+//  Created by zhujl on 2018/12/14.
+//  Copyright © 2018年 finstao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoView.
+FOUNDATION_EXPORT double PhotoViewVersionNumber;
+
+//! Project version string for PhotoView.
+FOUNDATION_EXPORT const unsigned char PhotoViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoView/PublicHeader.h>
+
+
